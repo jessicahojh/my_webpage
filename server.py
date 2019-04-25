f730e8f205808b02aad5acd3fb7e3345a4ce8626
Jessica Ho <jessica.ho.jh@gmail.com>
2019-04-25 18:10:37 +0000

    working on resume page

--- server.py
+++ server.py
@@ -1,9 +1,6 @@
 from jinja2 import StrictUndefined
 
-from flask import Flask, render_template, request, flash, redirect, session, jsonify
-
-
-from model import 
+from flask import Flask, render_template, request, flash, redirect
 
 import requests
 import json
@@ -54,8 +51,6 @@ if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the point
     # that we invoke the DebugToolbarExtension
     app.debug = True
-
-    connect_to_db(app)
 
     # Use the DebugToolbar
     #DebugToolbarExtension(app)
